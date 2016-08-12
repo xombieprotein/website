efb1fa38dce937805b0059d5feb61ad85b60b764
Matthew Broughton <m.broughton@griffithuni.edu.au>
2016-08-12 15:33:36 +1000

    Added MailChimp sign up form

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,6 +46,8 @@
 			<h2 class="text-center">Banana</h2>
 		</div>
 	</div>
+	<div class="row">
+	<div class="col-sm-8 col-sm-offset-2">	
 	<div class="panel panel-default">
 		<div class="panel-body">
 			
@@ -86,5 +88,7 @@
 			<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
 			<!--End mc_embed_signup-->
 		</div>
+	</div>
+	</div>
 	</div>
 @endsection
