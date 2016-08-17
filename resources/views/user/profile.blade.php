8dd98d9559dfd58f8ef875f2ba1567c5d24a7d83
Matthew Broughton <Matt@Matthews-iMac.local>
2016-08-17 11:48:14 +1000

    Altered layout of profile page

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -37,68 +37,92 @@
         	</div>
         </div>
     @else
-    	<nav class="nav nav-tabs">
-	    	<a href="{{ route('profile.edit', ['id' => Auth::user()->id]) }}">Edit</a>
-    	</nav>
     	<div class="row">
 			<div class="col-sm-2 col-lg-2 col-lg-offset-1" id="profileNav">
 				<!-- Nav tabs -->
 				<ul class="nav nav-pills nav-stacked" role="tablist">
-					<li role="presentation" class="active"><a href="#dashboard" aria-controls="dashboard" role="tab" data-toggle="pill">Dashboard</a></li>
-					<li role="presentation"><a href="#purchases" aria-controls="purchases" role="tab" data-toggle="pill">Purchases</a></li>
-					<li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="pill">Settings</a></li>
+					<li role="presentation" class="active"><a href="#dashboard" aria-controls="dashboard" role="tab" data-toggle="pill">Account Information</a></li>
+					<li role="presentation"><a href="#purchases" aria-controls="purchases" role="tab" data-toggle="pill">Purchase History</a></li>
 				</ul>
 			</div>
 			<div class="col-sm-10 col-lg-8">
         	<!-- Tab panes -->
 			<div class="tab-content">
 				<div role="tabpanel" class="tab-pane fade in active" id="dashboard">
-					<div class="col-sm-6">
+					<div class="col-sm-12">
 						<div class="panel panel-default">
 				        	<div class="panel-heading">
-					        	<h3 class="lead">Personal Information</h3>
+					        	<h3 class="lead text-center">Account Information</h3>
 				        	</div>
 				        	<div class="panel-body">
-					        	<h4><small>NAME</small> {{ Auth::user()->name }}</h4>
-					        	<h4><small>EMAIL</small> {{ Auth::user()->email }}</h4>
-					        	<h4><small>ADDRESS</small> {{ Auth::user()->email }}</h4>
+					        	<ul class="nav nav-pills">
+							    	<li class="pull-right"><a href="{{ route('profile.edit', ['id' => Auth::user()->id]) }}">Edit</a></li>
+						    	</ul>
+						    	<h3 class="lead text-center">User Credentials</h3>
+					        	<div class="row">
+						        	<div class="col-md-4 text-right">
+						        		<h4><small>NAME</small></h4>
+						        	</div>
+						        	<div class="col-md-6">
+										<h4>{{ Auth::user()->name }}</h4>
+									</div>
+									<div class="col-md-4 text-right">
+						        		<h4><small>EMAIL</small></h4>
+						        	</div>
+						        	<div class="col-md-6">
+										<h4>{{ Auth::user()->email }}</h4>
+									</div>
+									<div class="col-md-4 text-right">
+						        		<h4><small>PASSWORD</small></h4>
+						        	</div>
+						        	<div class="col-md-6">
+										<h4><a href="{{ url('/password/reset') }}">Reset Your Password</a></h4>
+									</div>
+								</div>
+					        	<hr>
+					        	<h3 class="lead text-center">Shipping Details</h3>
+					        	<div class="row">
+						        	<div class="col-md-4 text-right">
+						        		<h4><small>ADDRESS</small></h4>
+						        	</div>
+						        	<div class="col-md-6">
+										<h4>{{ Auth::user()->address }}</h4>
+									</div>
+									<div class="col-md-4 text-right">
+						        		<h4><small>SUBURB</small></h4>
+						        	</div>
+						        	<div class="col-md-6">
+										<h4>{{ Auth::user()->city }}, {{ Auth::user()->state }}</h4>
+									</div>
+									<div class="col-md-4 text-right">
+						        		<h4><small>POST CODE</small></h4>
+						        	</div>
+						        	<div class="col-md-6">
+										<h4>{{ Auth::user()->postalcode }}</h4>
+									</div>
+								</div>
+					        	<hr>
+					        	<div class="row">
+						        	<h3 class="lead text-center">Delete Account</h3>
+						        	<p class="text-center col-md-6 col-md-offset-3" style="margin-bottom: 1.5em">
+							        	Deleting your account will permanently remove your details and purchase history.
+							        	You can register with the same email again in the future, but you will not recover
+							        	your purchase history.
+						        	</p>
+						        	<div class="col-md-4 text-right"><h4><small >DELETE ACCOUNT</small></h4></div>
+							        <div class="col-md-6">	
+										<form method="POST" action="/profile/delete/{{ Auth::user()->id}}">
+				                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+				                            <input type="hidden" name="_method" value="DELETE" />
+				                            <button type="submit" class="btn btn-danger">
+				                                <i class="glyphicon glyphicon-trash"></i> Delete Account
+				                            </button>
+				                        </form>
+			                        </div>
+		                        </div>
 				        	</div>
 			        	</div>
 			        	<a></a>
-		        	</div>
-		        	<div class="col-sm-6">
-			        	<div class="panel panel-default">
-				        	<div class="panel-heading">
-					        	<h3 class="lead">Shipping Information</h3>
-				        	</div>
-				        	<div class="panel-body">
-					        	<h4><small>ADDRESS</small> {{ Auth::user()->address }}</h4>
-					        	<h4><small>SUBURB</small> {{ Auth::user()->city }}, {{ Auth::user()->state }}</h4>
-					        	<h4><small>POST CODE</small> {{ Auth::user()->postalcode }}</h4>
-				        	</div>
-			        	</div>
-		        	</div>
-		        	<div class="col-sm-6">
-			        	<div class="panel panel-default">
-				        	<div class="panel-heading">
-					        	<h3 class="lead">Payment Information</h3>
-				        	</div>
-				        	<div class="panel-body">
-					        	<p class="lead"></p>
-				        	</div>
-			        	</div>
-		        	</div>
-		        	<div class="col-sm-6">
-			        	<div class="panel panel-default">
-				        	<div class="panel-heading">
-					        	<h3 class="lead">My Orders</h3>
-				        	</div>
-				        	<div class="panel-body">
-					        	<h4><small>NAME</small> {{ Auth::user()->name }}</h4>
-					        	<h4><small>EMAIL</small> {{ Auth::user()->email }}</h4>
-					        	<h4><small>ADDRESS</small> {{ Auth::user()->email }}</h4>
-				        	</div>
-			        	</div>
 		        	</div>
 				</div>
 				<div role="tabpanel" class="tab-pane fade" id="purchases">
@@ -150,19 +174,6 @@
 			        	</tbody>
 		        	</table>
 	        	</div>
-				</div>
-				<div role="tabpanel" class="tab-pane fade" id="settings">
-					<div class="panel panel-default">
-						<div class="panel-body">
-							<form method="POST" action="/profile/delete/{{ Auth::user()->id}}">
-                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                            <input type="hidden" name="_method" value="DELETE" />
-                            <button type="submit" class="btn btn-danger">
-                                <i class="glyphicon glyphicon-trash"></i> Delete Account
-                            </button>
-                        </form>
-						</div>
-					</div>
 				</div>
 			</div>
 			</div>
