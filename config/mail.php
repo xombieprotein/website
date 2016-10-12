a5cb0181edc259803a301208756305e0c1a612d6
Matthew Broughton <Matt@Matthews-iMac.local>
2016-10-12 20:22:35 +1000

    Updated mail send settings

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'xombieprotein@outlook.com', 'name' => 'Xombie Protein'],
 
     /*
     |--------------------------------------------------------------------------
