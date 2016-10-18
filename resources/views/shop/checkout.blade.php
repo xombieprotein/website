387944546e04710746a5b7f56e1de98a9a3bf0a1
Matthew Broughton <Matt@Matthews-iMac.local>
2016-10-18 22:44:17 +1000

    Added an alternative checkout form for guest users. Doesn’t pre-fill any values in the form.

--- resources/views/shop/checkout.blade.php
+++ resources/views/shop/checkout.blade.php
@@ -16,7 +16,66 @@
 				</div>
 				<div class="panel-body">
 					@if (Auth::guest())
-						<h1 class="text-center">Please Sign In First</h1>
+						<h4>Your Total: ${{ $total }}</h4>
+					<div id="charge-error" class="alert alert-danger {{ !Session::has('error') ? 'hidden' : ''  }}" role="alert">
+						<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+		                {{ Session::get('error') }}
+		            </div>
+					<form action="{{ route('checkout') }}" method="post" id="checkout-form" autocomplete="on">
+						<div class="row">
+		                    <div class="col-xs-12">
+		                        <div class="form-group">
+		                            <label for="name">Name</label>
+		                            <input type="text" id="name" class="form-control" required name="name" placeholder="John Doe">
+		                        </div>
+		                    </div>
+		                    <div class="col-xs-12">
+		                        <div class="form-group">
+		                            <label for="address">Address</label>
+		                            <input type="text" id="address" class="form-control" required name="address" autocomplete="street-address" placeholder="Shipping Address">
+		                        </div>
+		                    </div>
+		                    <hr>
+		                    <div class="col-xs-12">
+		                        <div class="form-group">
+		                            <label for="card-name">Card Holder Name</label>
+		                            <input type="text" id="card-name" class="form-control" required>
+		                        </div>
+		                    </div>
+		                    <div class="col-xs-12">
+		                        <div class="form-group">
+		                            <label for="card-number">Credit Card Number</label>
+		                            <input type="text" id="card-number" class="form-control" required>
+		                        </div>
+		                    </div>
+		                    <div class="col-xs-12">
+		                        <div class="row">
+		                            <div class="col-xs-6 col-sm4">
+		                                <div class="form-group">
+		                                    <label for="card-expiry-month">Expiration Month</label>
+		                                    <input type="text" id="card-expiry-month" class="form-control" required>
+		                                </div>
+		                            </div>
+		                            <div class="col-xs-6">
+		                                <div class="form-group">
+		                                    <label for="card-expiry-year">Expiration Year</label>
+		                                    <input type="text" id="card-expiry-year" class="form-control" required>
+		                                </div>
+		                            </div>
+		                        </div>
+		                    </div>
+		                    <div class="col-xs-12">
+		                        <div class="form-group">
+		                            <label for="card-cvc">CVC</label>
+		                            <input type="text" id="card-cvc" class="form-control" required>
+		                        </div>
+		                    </div>
+		                </div>
+		                {{ csrf_field() }}
+		                <div class="panel-body"><button type="submit" class="btn btn-success btn-block btn-lg"><strong>Buy now</strong></button></div>
+					</form>
 					@else
 					<h4>Your Total: ${{ $total }}</h4>
 					<div id="charge-error" class="alert alert-danger {{ !Session::has('error') ? 'hidden' : ''  }}" role="alert">
